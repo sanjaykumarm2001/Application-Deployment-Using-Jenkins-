d3b2270e810c376fe62b13e697d718b746553a5c
sanjaykumarm2001 <sanjai202001@gmail.com>
2024-08-19 21:37:17 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 # Ensure proper spacing in conditional statements
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
     chmod +x build.sh
-    build/build.sh
+    ./build.sh
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     docker tag react-app sanjaykumar2001/dev
     docker push sanjaykumar2001/dev
 
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
     chmod +x build.sh
-    build/build.sh
+    ./build.sh
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     docker tag react-app sanjaykumar2001/prod-repo
     docker push sanjaykumar2001/prod-repo
