b2687509db92641379661bb3c121496c4e859199
sanjaykumarm2001 <sanjai202001@gmail.com>
2024-08-19 13:05:03 +0530

    changes in jenkinsfile

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,12 +1,19 @@
-#!bin/bash
-if [[GIT.BRANCH == "origin/dev" ]];then
-    sh "chmod +x build.sh"
-    sh "build/build.sh"
+#!/bin/bash
+
+# Ensure proper spacing in conditional statements
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    chmod +x build/build.sh
+    build/build.sh
     docker tag react-app sanjaykumar2001/dev
     docker push sanjaykumar2001/dev
 
-elif[[GIT.BRANCH=="origin/master"]];then
-    sh "chmod +x build/build.sh"
-    sh "build/build.sh"
+elif [[ $GIT_BRANCH == "origin/master" ]]; then
+    chmod +x build/build.sh
+    build/build.sh
     docker tag react-app sanjaykumar2001/prod-repo
     docker push sanjaykumar2001/prod-repo
+
+else
+    echo "Branch $GIT_BRANCH is not recognized. Exiting."
+    exit 1
+fi
