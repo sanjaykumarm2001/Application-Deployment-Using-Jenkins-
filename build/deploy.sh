349224ab11c03855b570e0bc7adc0b9e931a9b9c
sanjaykumarm2001 <sanjai202001@gmail.com>
2024-08-19 12:52:34 +0530

    changes in jenkinsfile

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 if [[GIT.BRANCH == "origin/dev" ]];then
-    sh "chmod +x build/build.sh"
-    sh "./build/build.sh"
+    sh "chmod +x build.sh"
+    sh "build/build.sh"
     docker tag react-app sanjaykumar2001/dev
     docker push sanjaykumar2001/dev
 
 elif[[GIT.BRANCH=="origin/master"]];then
     sh "chmod +x build/build.sh"
-    sh "./build/build.sh"
+    sh "build/build.sh"
     docker tag react-app sanjaykumar2001/prod-repo
     docker push sanjaykumar2001/prod-repo
